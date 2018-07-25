aaf404fb4a958d8d6e189c87122f3b9c68d7d7d3
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-24 19:05:44 -0700

    got the speed to work

--- SpeedTracker/SpeedTracker/ViewController.swift
+++ SpeedTracker/SpeedTracker/ViewController.swift
@@ -5,37 +5,40 @@
 //  Created by Sakura Rapolu on 7/24/18.
 //  Copyright © 2018 MakeSchool. All rights reserved.
 //
-
 import UIKit
-import CoreMotion
+import CoreLocation
 
-class ViewController: UIViewController {
-    
-    var motionManager = CMMotionManager()
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        motionManager.accelerometerUpdateInterval = 0.2
+    class ViewController: UIViewController, CLLocationManagerDelegate {
+        let locationManager = CLLocationManager()
         
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-            if let myData = data
-            {
-                //assume velocity at the beginning is 0?
-                print(myData)
+        @IBOutlet weak var speedLabel: UILabel!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            locationManager.delegate = self
+            if NSString(string:UIDevice.current.systemVersion).doubleValue > 8 {
+                locationManager.requestAlwaysAuthorization()
+            }
+            locationManager.desiredAccuracy=kCLLocationAccuracyBest
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            var speed: CLLocationSpeed = CLLocationSpeed()
+            speed = (locationManager.location?.speed)!
+            speedLabel.text = String(speed)
+        }
+    
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            if status != CLAuthorizationStatus.denied{
+                locationManager.startUpdatingLocation()
             }
         }
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
