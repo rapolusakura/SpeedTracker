2295abbd544adf3feb18cbae7ed4f74c99bb454a
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-26 11:38:02 -0700

    added trip struct

---
+++ SpeedTracker/SpeedTracker/Models/Trip.swift
@@ -0,0 +1,18 @@
+//
+//  Trip.swift
+//  SpeedTracker
+//
+//  Created by Sakura Rapolu on 7/26/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+struct Trip {
+    let maxSpeed: Double
+    //let minSpeed: Double
+    let distanceTraveled: Double
+    let totalTime: Double
+    let timeSaved: Double
+
+}
